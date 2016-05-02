b93c55ed5fd73f5ee657dc7400476188571771ae
Sean <seanliew90@icloud.com>
2016-05-02 16:07:06 +1000

    added template

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+// Generated on 2015-11-08 using generator-leaflet 0.0.17
+
+var gulp = require('gulp');
+var open = require('open');
+var wiredep = require('wiredep').stream;
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+// Styles
+
+// Scripts
+
+// HTML
+
+// Images
+
+// Clean
+
+// Build
+
+// Default task
+
+// Connect
+
+// Open
+
+// Inject Bower components
+
+// Watch
